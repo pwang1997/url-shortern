20f94fd38a748a1e475727e66882ec9b55edc9de
Zhengliang Wang <puck19970418@gmail.com>
2024-10-20 21:54:20 -0400

    update navbar

--- src/components/compound/navbar.tsx
+++ src/components/compound/navbar.tsx
@@ -1,10 +1,21 @@
+import { Button } from "../ui/button";
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "../ui/navigation-menu";
 
 export default async function TopNav() {
     return (
-        <header className='w-full sticky top-0 backdrop-blur transition-[background-color,border-width] border-x-0 flex justify-center z-10 pt-4'>
-            <nav className='' aria-label='Global'>
-                <NavigationMenu>
+        <header className='w-full sticky top-0 border-x-0 flex justify-center z-10 pt-2'>
+            <nav className='container w-full flex items-center justify-between' aria-label='Global'>
+                <NavigationMenu className="flex justify-between">
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink href="/" className={navigationMenuTriggerStyle()}>
+                                Home Icon
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+
+                <NavigationMenu className="flex justify-between">
                     <NavigationMenuList>
                         <NavigationMenuItem>
                             <NavigationMenuLink href="/" className={navigationMenuTriggerStyle()}>
@@ -13,8 +24,8 @@ export default async function TopNav() {
                         </NavigationMenuItem>
 
                         <NavigationMenuItem>
-                            <NavigationMenuLink href="https://github.com/pwang1997" className={navigationMenuTriggerStyle()}>
-                                GitHub
+                            <NavigationMenuLink href="/generate" className={`${navigationMenuTriggerStyle()} pt-0 pb-0 pl-0 pr-0`}>
+                                <Button>Get Started</Button>
                             </NavigationMenuLink>
                         </NavigationMenuItem>
                     </NavigationMenuList>
