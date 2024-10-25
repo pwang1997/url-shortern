3bd34770fe3ff7b7bde3306fa8867f016a4c609c
Zhengliang Wang <puck19970418@gmail.com>
2024-10-24 21:04:27 -0400

    update landing page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,25 +12,23 @@ export default async function Home() {
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20 hover:shadow-lg"
+              href="https://url-shortern.vercel.app/generate"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">First Steps →</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+                Url Shortener is a tool to shorten a long link and create a short URL easy to share on sites, chat and emails.
               </div>
             </Link>
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20 hover:shadow-lg"
+              href="https://github.com/pwang1997/url-shortern"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">Documentation →</h3>
               <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+                Get to know about the source code of Url Shortener.
               </div>
             </Link>
           </div>
